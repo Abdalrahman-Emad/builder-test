78a23e330b30fc1af50034c41c457a4d6b76731a
Mostafa El-Sayed <73605018+jaycelol10@users.noreply.github.com>
2026-05-13 13:16:46 +0300

    Update App.tsx

--- html bulder2/src/App.tsx
+++ html bulder2/src/App.tsx
@@ -10,3 +10,62 @@ function App() {
 }
 
 export default App
+
+
+
+Object { id: 1, name: "temp1", description: "ecec", schemaType: null, createdAt: "2026-05-13T11:56:43.64557", updatedAt: "2026-05-13T12:57:27.338328", createdBy: "campaign-inputter", updatedBy: "campaign-inputter", contents: (1) […], smsTemplates: (1) […], … }
+​
+contents: Array [ {…} ]
+​
+createdAt: "2026-05-13T11:56:43.64557"
+​
+createdBy: "campaign-inputter"
+​
+description: "ecec"
+​
+id: 1
+​
+inboxTemplates: Array [ {…} ]
+​
+​
+0: Object { languageId: 1, languageCode: "ar", subject: "asa", … }
+​
+​
+​
+languageCode: "ar"
+​
+​
+​
+languageId: 1
+​
+​
+​
+subject: "asa"
+​
+​
+​
+text: "wdwed"
+​
+​
+​
+<prototype>: Object { … }
+​
+​
+length: 1
+​
+​
+<prototype>: Array []
+​
+name: "temp1"
+​
+pushTemplates: Array [ {…} ]
+​
+schemaType: null
+​
+smsTemplates: Array [ {…} ]
+​
+updatedAt: "2026-05-13T12:57:27.338328"
+​
+updatedBy: "campaign-inputter"
+​
+<prototype>: Object { … }
