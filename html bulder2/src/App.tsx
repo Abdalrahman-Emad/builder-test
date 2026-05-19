09555cb3578ed03c69abdf63954e512d705a322c
Abdulrahman Emad <116901699+Abdalrahman-Emad@users.noreply.github.com>
2026-05-19 09:47:47 +0300

    Update App.tsx

--- html bulder2/src/App.tsx
+++ html bulder2/src/App.tsx
@@ -33,6 +33,10 @@ Feature: Campaign Labels
       }
       """
     Then the campaign portal response status should be 201
+    And I save campaign portal response field "id" as "#camp1"
+
+    When I get campaign portal campaign "#camp1"
+    Then the campaign portal response status should be 200
     And the campaign portal response field "labels[0]" should be "ANNOUNCEMENT"
     And the campaign portal response field "labels[1]" should be "TRANSACTIONAL"
 
@@ -59,6 +63,7 @@ Feature: Campaign Labels
         "labels": ["ANNOUNCEMENT"]
       }
       """
+    Then the campaign portal response status should be 201
     And I save campaign portal response field "id" as "#camp1"
 
     When I get campaign portal campaign "#camp1"
@@ -88,3 +93,7 @@ Feature: Campaign Labels
       }
       """
     Then the campaign portal response status should be 201
+    And I save campaign portal response field "id" as "#camp1"
+
+    When I get campaign portal campaign "#camp1"
+    Then the campaign portal response status should be 200
